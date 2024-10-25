ed325b72129adeeb5c14c5482125330924b74f10
Andrea <andren151@hotmail.com>
2024-10-25 16:53:14 +0200

    45

--- scripts/label_approved.py
+++
@@ -1,8 +0,0 @@
-def label_approved(status):
-    if status == "approved":
-        return "Label is approved"
-    elif status == "pending":
-        return "Label is pending"
-    else:
-        return "Label is not approved"
-
